b870c53194c276283111cc2b89f9a98e41e49628
XiaoPeng <zxpcs0x3@gmail.com>
2024-06-26 13:21:08 +0800

    Done all the trains

---
+++ 工业蒸汽量预测/get_test.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+# 下载好数据集
+
+wget http://tianchi-media.oss-cn-beijing.aliyuncs.com/DSW/Industrial_Steam_Forecast/zhengqi_test.txt
+wget http://tianchi-media.oss-cn-beijing.aliyuncs.com/DSW/Industrial_Steam_Forecast/zhengqi_train.txt
